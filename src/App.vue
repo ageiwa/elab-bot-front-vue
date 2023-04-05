15d0d7338ed209256d159f725323d2f62c76c1dd
Vlad Ageysha <ander123fakk@gmail.com>
2023-04-05 14:18:22 +0300

    Moved img

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,17 @@
 
     const products = [
         {
-            img: '/public/box1.jpg',
+            img: '/src/assets/box1.jpg',
             amount: 1,
             price: 250
         },
         {
-            img: '/public/box2.jpg',
+            img: '/src/assets/box2.jpg',
             amount: 2,
             price: 350
         },
         {
-            img: '/public/box3.jpg',
+            img: '/src/assets/box3.jpg',
             amount: 3,
             price: 450
         },
