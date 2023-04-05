5afb49bdc286711f0344659af83e288b3d27f2ae
Vlad Ageysha <ander123fakk@gmail.com>
2023-04-05 14:15:36 +0300

    Moved rep

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+    import Product from '/src/components/Product/Product.vue'
+
+    const products = [
+        {
+            img: '/public/box1.jpg',
+            amount: 1,
+            price: 250
+        },
+        {
+            img: '/public/box2.jpg',
+            amount: 2,
+            price: 350
+        },
+        {
+            img: '/public/box3.jpg',
+            amount: 3,
+            price: 450
+        },
+    ]
+</script>
+
+<template>
+    <div class="container">
+        <Product v-for="product in products"
+            :img="product.img"
+            :amount="product.amount"
+            :price="product.price"
+        />
+    </div>
+</template>
---
+++ src/components/Product/Product.vue
@@ -0,0 +1,24 @@
+<script setup>
+    import './style.scss'
+
+    const props = defineProps({
+        img: String,
+        amount: Number,
+        price: Number
+    })
+
+</script>
+
+<template>
+    <div class="product">
+        <div class="product__img">
+            <img :src="props.img">
+        </div>
+        <div class="product__info">
+            <p class="info__title">Коробка</p>
+            <p class="info__title">Количество: {{ props.amount }} шт.</p>
+            <p class="info__title">Цена: {{ props.price }} руб.</p>
+        </div>
+        <button class="product__add-to-cart">Добавить в корзину</button>
+    </div>
+</template>
